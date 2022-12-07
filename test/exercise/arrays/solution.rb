e8b7318734d5c9fcb876d7c47007871c6186d0a2
Dmitry Kabanov <dkabanov6@gmail.com>
2022-12-07 07:58:51 +0500

    arrays/replace

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,8 +1,15 @@
 module Exercise
   module Arrays
     class << self
+      def find_max_item(array)
+        max_item = array.first
+        array.each { |item| max_item = item if item > max_item }
+        max_item
+      end
+
       def replace(array)
-        array
+        max_item = find_max_item(array)
+        array.map { |item| item.negative? ? item : max_item }
       end
 
       def search(_array, _query)
