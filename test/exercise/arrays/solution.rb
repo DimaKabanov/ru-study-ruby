1b1ca7a1a675f65c659fea925dee7e811d9be6d5
Dmitry Kabanov <dkabanov6@gmail.com>
2022-12-08 07:11:45 +0500

    arrays/search

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -12,8 +12,26 @@ module Exercise
         array.map { |item| item.negative? ? item : max_item }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        return -1 if array.count.zero?
+
+        iter = lambda do |start_index, end_index|
+          return -1 if start_index > end_index
+
+          middle_index = ((start_index + end_index) / 2).floor
+          current_item = array[middle_index]
+
+          return middle_index if current_item == query
+
+          return iter.call(start_index, middle_index - 1) if current_item > query
+
+          iter.call(middle_index + 1, end_index)
+        end
+
+        smallest_index = 0
+        highest_index = array.count - 1
+
+        iter.call(smallest_index, highest_index)
       end
     end
   end
