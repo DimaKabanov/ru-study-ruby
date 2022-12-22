c81c1b1bca0cbdc4fb6c8e08988cc1dad2c7b4a8
Dmitry Kabanov <dkabanov6@gmail.com>
2022-12-22 06:25:07 +0500

    fp:rating

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,16 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(films)
+        ratings = films
+                  .reject { |film| film['country'].nil? }
+                  .reject { |film| film['country'].strip.split(',').count < 2 }
+                  .reject { |film| film['rating_kinopoisk'].to_i.zero? }
+                  .map { |film| film['rating_kinopoisk'].to_f }
+
+        ratings_sum = ratings.reduce(0) { |sum, rating| sum + rating }
+
+        ratings_sum / ratings.count
       end
 
       def chars_count(_films, _threshold)
